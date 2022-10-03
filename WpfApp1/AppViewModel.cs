fda79169ec84aeef802e0712124c2d9d76ef357d
Mike Christiansen <37249876+binarycow@users.noreply.github.com>
2022-10-03 17:02:58 -0400

    Basic project (#1)

---
+++ WpfApp1/AppViewModel.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace WpfApp1;
+
+
+
+public class AppViewModel : ObservableObject
+{
+    public AppViewModel()
+    {
+        this.IncrementCommand = new RelayCommand<string>(Increment, CanIncrement);
+    }
+    public IRelayCommand<string> IncrementCommand { get; }
+    private void Increment(string? parameter)
+    {
+        if (int.TryParse(parameter, out var amount))
+            Value += amount;
+    }
+
+    private bool CanIncrement(string? arg)
+    {
+        return int.TryParse(arg, out var amount)
+            && this.Value + amount < 10;
+    }
+
+    private int value;
+    public int Value
+    {
+        get => this.value;
+        set
+        {
+            if (this.SetProperty(ref this.value, value))
+            {
+                this.IncrementCommand.NotifyCanExecuteChanged();
+            }
+        }
+    }
+}
